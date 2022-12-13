9a9dbc93ba05e14fc64383029ddc50e83308a073
薛晴 <2310109853@qq.com>
2022-12-13 14:33:03 +0800

    111

---
+++ src/main/java/cn/edu/uzz/mapper/ProductInfoMapper.java
@@ -0,0 +1,39 @@
+package cn.edu.uzz.mapper;
+
+import cn.edu.uzz.pojo.ProductInfo;
+import cn.edu.uzz.pojo.ProductInfoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ProductInfoMapper {
+    int countByExample(ProductInfoExample example);
+
+    int deleteByExample(ProductInfoExample example);
+
+    int deleteByPrimaryKey(Integer pId);
+
+    int insert(ProductInfo record);
+
+    int insertSelective(ProductInfo record);
+
+    List<ProductInfo> selectByExample(ProductInfoExample example);
+
+    ProductInfo selectByPrimaryKey(Integer pId);
+
+    int updateByExampleSelective(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByExample(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByPrimaryKeySelective(ProductInfo record);
+
+    int updateByPrimaryKey(ProductInfo record);
+
+    /**
+     * 批量删除
+     *
+     * @param ids 多个id同时删除
+     * @return int 受影响的行数
+     */
+    int deleteBatch(String[] ids);
+}
